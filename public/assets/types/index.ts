1b174bfda850c478aa580e8fdf632c2e185bb7b5
TerSimaN <mcsiman998@gmail.com>
2024-05-27 17:46:40 +0300

    Added pokemon type icons

---
+++ public/assets/types/index.ts
@@ -0,0 +1,41 @@
+import noraml_type from './TeraGem_Normal.png';
+import fighting_type from './TeraGem_Fighting.png';
+import flying_type from './TeraGem_Flying.png';
+import poison_type from './TeraGem_Poison.png';
+import ground_type from './TeraGem_Ground.png';
+import rock_type from './TeraGem_Rock.png';
+import bug_type from './TeraGem_Bug.png';
+import ghost_type from './TeraGem_Ghost.png';
+import steel_type from './TeraGem_Steel.png';
+import fire_type from './TeraGem_Fire.png';
+import water_type from './TeraGem_Water.png';
+import grass_type from './TeraGem_Grass.png';
+import electric_type from './TeraGem_Electric.png';
+import psychic_type from './TeraGem_Psychic.png';
+import ice_type from './TeraGem_Ice.png';
+import dragon_type from './TeraGem_Dragon.png';
+import dark_type from './TeraGem_Dark.png';
+import fairy_type from './TeraGem_Fairy.png';
+import stellar_type from './TeraGem_Stellar.png';
+
+export {
+    noraml_type,
+    fighting_type,
+    flying_type,
+    poison_type,
+    ground_type,
+    rock_type,
+    bug_type,
+    ghost_type,
+    steel_type,
+    fire_type,
+    water_type,
+    grass_type,
+    electric_type,
+    psychic_type,
+    ice_type,
+    dragon_type,
+    dark_type,
+    fairy_type,
+    stellar_type
+}
