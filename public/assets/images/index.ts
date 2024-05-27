4770cb76bc69f33ea01cdcfd3218989ea443c540
TerSimaN <mcsiman998@gmail.com>
2024-05-27 17:46:11 +0300

    Added images

---
+++ public/assets/images/index.ts
@@ -0,0 +1,9 @@
+import pokemon_logo from './International_Pokémon_logo.svg';
+import poke_api_logo from './pokeapi.svg';
+import no_image from './no-image.svg';
+
+export {
+    pokemon_logo,
+    poke_api_logo,
+    no_image
+}
