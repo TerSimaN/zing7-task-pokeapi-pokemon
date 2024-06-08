ae6b6f28e38ccd658ee06046ea6db914a517c33c
TerSimaN <mcsiman998@gmail.com>
2024-06-08 19:09:49 +0300

    Refactored code

--- src/components/layout/Pagination.tsx
+++ src/components/layout/Pagination.tsx
@@ -7,17 +7,38 @@ import PreviousPageButton from "../ui/PreviousPageButton";
 import NextPageButton from "../ui/NextPageButton";
 
 const Pagination = (props: { offset: number, entries: number }) => {
-    const [current, setCurrent] = useState(0);
-    const [currentPage, setCurrentPage] = useState(1);
+    const [onPage, setOnPage] = useState(0);
     const [previousPage, setPreviousPage] = useState(1);
-    const [nextPage, setNextPage] = useState(currentPage + 1);
+    const [nextPage, setNextPage] = useState(2);
 
     let count: number = props.entries;
     let fromEntry: number = 1 + props.offset;
     let toEntry: number = (50 + props.offset) > count ? count : (50 + props.offset);
     let pageNumbers = createPageNumbersArray(count);
 
-    function handlePageChange(newPage: number) {
+    function handlePreviousPage() {
+        let nextValue = onPage - 1;
+        if (nextValue > -1) {
+            setOnPage(nextValue);
+        }
+        updatePreviousAndNextPage(previousPage);
+    }
+
+    function handleNextPage() {
+        let nextValue = onPage + 1;
+        if (nextValue < pageNumbers.length) {
+            setOnPage(nextValue);
+        }
+        updatePreviousAndNextPage(nextPage);
+    }
+
+    function handleCurrentPage(pageNumber: { href: string, number: string }, index: number) {
+        let pageValue = parseInt(pageNumber.number);
+        setOnPage(index);
+        updatePreviousAndNextPage(pageValue);
+    }
+
+    function updatePreviousAndNextPage(newPage: number) {
         let newPreviousPage = newPage - 1;
         let newNextPage = newPage + 1;
 
@@ -30,7 +51,6 @@ const Pagination = (props: { offset: number, entries: number }) => {
         if (newNextPage <= pageNumbers.length) {
             setNextPage(newNextPage);
         }
-        setCurrentPage(newPage);
     }
 
     return (
@@ -39,13 +59,7 @@ const Pagination = (props: { offset: number, entries: number }) => {
                 Showing <span className="font-semibold text-gray-900">{fromEntry}</span> to <span className="font-semibold text-gray-900">{toEntry}</span> of <span className="font-semibold text-gray-900">{count}</span> Entries
             </span>
             <nav className="flex items-center md:justify-normal justify-between -space-x-px font-inter text-sm sm:mt-0 mt-4 h-8 sm:max-w-sm lg:max-w-xl 2xl:max-w-none">
-                <button onClick={() => {
-                    let value = current - 1;
-                    if (value > -1) {
-                        setCurrent(value);
-                    }
-                    handlePageChange(previousPage);
-                }}>
+                <button onClick={handlePreviousPage}>
                     <PreviousPageButton href={`?page=${previousPage}`} />
                 </button>
                 <ul className="hidden md:flex items-center -space-x-px overflow-x-auto">
@@ -54,27 +68,17 @@ const Pagination = (props: { offset: number, entries: number }) => {
                             <Link
                                 href={pageNumber.href}
                                 className={`flex items-center justify-center leading-tight border 
-                                    ${current === index ?
+                                    ${onPage === index ?
                                         `text-blue-500 bg-blue-50 border-blue-300 hover:bg-blue-100 hover:text-blue-700` :
                                         `text-gray-500 bg-white border-gray-300 hover:bg-gray-100 hover:text-gray-700`
                                     } px-3 h-8`}
-                                onClick={() => {
-                                    let pageValue = parseInt(pageNumber.number);
-                                    setCurrent(index);
-                                    handlePageChange(pageValue);
-                                }}>
+                                onClick={() => handleCurrentPage(pageNumber, index)}>
                                 {pageNumber.number}
                             </Link>
                         </li>
                     ))}
                 </ul>
-                <button onClick={() => {
-                    let value = current + 1;
-                    if (value < pageNumbers.length) {
-                        setCurrent(value);
-                    }
-                    handlePageChange(nextPage);
-                }}>
+                <button onClick={handleNextPage}>
                     <NextPageButton href={`?page=${nextPage}`} />
                 </button>
             </nav>
