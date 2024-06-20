9560e1fb30fc59d027d968463548c1114863c2dd
TerSimaN <mcsiman998@gmail.com>
2024-06-20 19:37:07 +0300

    Refactored code, added TODO

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,70 @@
+// "use client";
+
 import Filter from "@/components/layout/Filter";
 import Pagination from "@/components/layout/Pagination";
 import PokemonGrid from "@/components/layout/PokemonGrid";
 import PokemonGridLoading from "@/components/loading/PokemonGridLoading";
+import { getGenerationPokemonSpeciesArray } from "@/lib/actions/generation";
 import { getPokemonList } from "@/lib/actions/pokemon";
 import { getTypePokemonArray } from "@/lib/actions/pokemonType";
-import { getFiltersFromParams, getOffsetFromProps } from "@/lib/utils";
-import { Suspense } from "react";
+import { getFiltersFromParams, getOffsetFromParams } from "@/lib/utils";
+import { Suspense, useEffect, useState } from "react";
 
 type Params = {
   searchParams: {
     page: string | undefined,
-    filter: string | undefined
+    [key: string]: string | string[] | undefined
   }
 }
 
 export default async function Home(props: Params) {
-  let offset: number = getOffsetFromProps(props.searchParams.page);
-  let filter: string | string[] = getFiltersFromParams(props.searchParams.filter);
+  // const [pokemonList, setPokemonList] = useState({} as PokeAPI.Utility.NamedAPIResourceList);
+  // const [typePokemonArray, setTypePokemonArray] = useState([] as PokeAPI.Types.TypePokemon[]);
+  // const [resourceArray, setResourceArray] = useState([] as PokeAPI.Utility.NamedAPIResource[]);
+  // const [entries, setEntries] = useState(0);
+  // const [searchValue, setSearchValue] = useState("");
+
+  let offset: number = getOffsetFromParams(props.searchParams.page);
+  let typeFilter: string | string[] = getFiltersFromParams(props.searchParams["type"]);
+  let genFilter: string | string[] = getFiltersFromParams(props.searchParams["gen"]);
+
   let entries: number = 0;
-
   let resourceArray: PokeAPI.Utility.NamedAPIResource[] = [];
+  let typePokemonArray: PokeAPI.Types.TypePokemon[] = [];
+  let generationPokemonSpeciesArray: PokeAPI.Utility.NamedAPIResource[] = [];
   let pokemonList: PokeAPI.Utility.NamedAPIResourceList;
-  let typePokemonArray = (await getTypePokemonArray(filter));
 
-  if (typePokemonArray.length) {
+  // useEffect(() => {
+  //   if (filter) {
+  //     getTypePokemonArray(filter).then((data) => {
+  //       setTypePokemonArray([...typePokemonArray, ...data]);
+  //     });
+  //   } else {
+  //     getPokemonList(offset).then((data) => {
+  //       setPokemonList(data);
+  //       setResourceArray([...resourceArray, ...data.results]);
+  //       setEntries(data.count);
+  //     });
+  //   }
+  // }, []);
+
+  if (typeFilter.length) {
+    typePokemonArray = (await getTypePokemonArray(typeFilter)) as PokeAPI.Types.TypePokemon[];
     for (const typePokemon of typePokemonArray) {
       if (!resourceArray.includes(typePokemon.pokemon)) {
         resourceArray.push(typePokemon.pokemon);
       }
     }
+
+    entries = resourceArray.length;
+  } else if (genFilter.length) {
+    generationPokemonSpeciesArray = (await getGenerationPokemonSpeciesArray(genFilter)) as PokeAPI.Utility.NamedAPIResource[];
+    for (const pokemonSpecies of generationPokemonSpeciesArray) {
+      if (!resourceArray.includes(pokemonSpecies)) {
+        resourceArray.push(pokemonSpecies);
+      }
+    }
+
     entries = resourceArray.length;
   } else {
     pokemonList = (await getPokemonList(offset)) as PokeAPI.Utility.NamedAPIResourceList;
@@ -38,11 +74,34 @@ export default async function Home(props: Params) {
 
   return (
     <main className="flex flex-col sm:justify-between sm:px-24 xl:pt-12 pt-4 min-h-screen">
-      <Filter />
+      <Filter searchValue={"searchValue"} onSearchBarChange={"setSearchValue"} />
       <Suspense fallback={<PokemonGridLoading />}>
-        <PokemonGrid resourceArray={resourceArray} />
+        <PokemonGrid resourceArray={resourceArray} searchFilter={"searchValue"} />
       </Suspense>
       <Pagination offset={offset} entries={entries} />
     </main>
   );
 }
+
+/** TODO:
+ * DO NOT Try to make Home page.tsx a client component (UNLESS Really needed),
+ *  because it breaks fetch requests
+ * 
+ * Finish filtering of pokemon by type filter and searching w/ autocomplete
+ * Finish filtering of pokemon by pokemon generation and type filter
+ * 
+ * Use https://flowbite.com/docs/components/skeleton/ as:
+ *  LoadingSkeleton.tsx
+ *  a loading page when waiting for data
+ *  fetched from filtering by type or w/ search to load
+ * 
+ * Change pagination bar to not scroll, but show
+ *  '[1][2][3][...][25][26][27]' page numbers instead
+ * 
+ * (May not be possible) Try to fix currentPage, nextPage and previousPage values
+ * when returning from /pokemon/[id] page via the Go Back browser button
+ * 
+ * Decide what to do with 'stellar, unknown and shadow' types
+ * 
+ * Write here if more TODO's are needed
+ */
--- src/components/layout/PokemonGrid.tsx
+++ src/components/layout/PokemonGrid.tsx
@@ -1,16 +1,75 @@
+// "use client";
+
 import Image from "next/image"
 import Link from "next/link"
 import { no_image } from "../../../public/assets/images"
-import { getIconByTypeName } from "@/lib/utils"
-import { getPokemonByName } from "@/lib/actions/pokemon"
+import { getIconByTypeName, getPokemonIdFromResultUrl } from "@/lib/utils"
+import { getPokemonArray, getPokemonById, getPokemonByName } from "@/lib/actions/pokemon"
+import { useEffect, useState } from "react";
 
-const PokemonGrid = (props: { resourceArray: PokeAPI.Utility.NamedAPIResource[] }) => {
+const PokemonGrid = (props: { resourceArray: PokeAPI.Utility.NamedAPIResource[], searchFilter: string }) => {
+    // const [pokemonArray, setPokemonArray] = useState([] as PokeAPI.Pokemon.Pokemon[]);
+
+    // useEffect(() => {
+    //     getPokemonArray(props.resourceArray).then((data) => {
+    //         setPokemonArray([...pokemonArray, ...data]);
+    //     });
+    // }, []);
+
+    // console.log(props.resourceArray);
+
     return (
         <section className="bg-white">
             <div className="lg:px-6 sm:px-4 px-8 lg:py-16 py-8 mx-auto max-w-screen-xl">
                 <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+                    {/* {pokemonArray.map((pokemon, index) => {
+                        if (
+                            pokemon.name.toLowerCase().indexOf(
+                                props.searchFilter.toLowerCase()
+                            ) === -1
+                        ) {
+                            return;
+                        }
+
+                        return (
+                            <article key={index} className="bg-white border border-gray-200 rounded-lg shadow-md p-4">
+                                <Link href={`/pokemon/${pokemon.id}`}>
+                                    <Image
+                                        src={pokemon.sprites.front_default ?? no_image}
+                                        alt="pokemon image"
+                                        width={288}
+                                        height={288}
+                                        className="rounded-lg mb-5"
+                                    />
+                                </Link>
+                                <h2 className="font-inter font-bold text-2xl tracking-tight text-gray-900 capitalize mb-2">
+                                    <Link href={`/pokemon/${pokemon.id}`}>{pokemon.name}</Link>
+                                </h2>
+                                <div className="flex items-center gap-2 mb-5">
+                                    {pokemon.types.map((type, index) => {
+                                        const typeIcon = getIconByTypeName(type.type.name);
+                                        return (
+                                            <span key={index} className="inline-flex items-center bg-gray-100 rounded font-inter font-medium sm:text-sm text-base text-gray-500 pl-1.5 pr-2 py-1 capitalize select-none hover:bg-gray-200 hover:text-gray-600">
+                                                <Image src={typeIcon.typeIconSrc} alt={typeIcon.typeIconAlt} className="sm:size-7 size-9 me-1" />
+                                                {type.type.name}
+                                            </span>
+                                        )
+                                    })}
+                                </div>
+                                <div className="flex justify-between items-center">
+                                    <Link href={`/pokemon/${pokemon.id}`} className="inline-flex items-center font-inter font-medium text-gray-600 hover:underline">
+                                        Read more
+                                        <svg className="ml-2 size-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                            <path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd" />
+                                        </svg>
+                                    </Link>
+                                </div>
+                            </article>
+                        )
+                    })} */}
                     {props.resourceArray.map(async (resource, index) => {
-                        let pokemon = (await getPokemonByName(resource.name)) as PokeAPI.Pokemon.Pokemon;
+                        let pokemonId: string = getPokemonIdFromResultUrl(resource.url);
+                        let pokemon = (await getPokemonById(pokemonId)) as PokeAPI.Pokemon.Pokemon;
                         return (
                             <article key={index} className="bg-white border border-gray-200 rounded-lg shadow-md p-4">
                                 <Link href={`/pokemon/${pokemon.id}`}>
