28c391ec22c79469e1271e303e2e18afa6b41cce
TerSimaN <mcsiman998@gmail.com>
2024-06-20 19:39:03 +0300

    Refactored code, added generation filter

--- src/components/layout/Filter.tsx
+++ src/components/layout/Filter.tsx
@@ -2,11 +2,11 @@
 
 import Image from "next/image"
 import { useState } from "react"
-import { types } from "@/config/constants"
-import { handleTypeChange } from "@/lib/actions/pokemonType"
+import { generations, types } from "@/config/constants"
 import { searchFilter } from "@/lib/actions/pokemon"
+import { handleFilterChange } from "@/lib/actions/filter"
 
-const Filter = () => {
+const Filter = (props: { searchValue: string, onSearchBarChange: any }) => {
     const [searchOpen, setSearchOpen] = useState(false);
     const [filterOpen, setFilterOpen] = useState(false);
 
@@ -21,7 +21,16 @@ const Filter = () => {
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                                 </svg>
                             </div>
-                            <input className="block bg-gray-50 border border-gray-300 rounded-lg pl-10 pr-4 py-2 w-full placeholder:font-inter placeholder:text-sm placeholder:text-gray-900/50" id="search-bar" name="search-bar" type="search" placeholder="Search" />
+                            <input
+                                className="block bg-gray-50 border border-gray-300 rounded-lg pl-10 pr-4 py-2 w-full placeholder:font-inter placeholder:text-sm placeholder:text-gray-900/50"
+                                id="search-bar"
+                                name="search-bar"
+                                type="search"
+                                placeholder="Search"
+                                value={props.searchValue}
+                                onChange={(e) => props.onSearchBarChange(e.target.value)}
+                                disabled
+                            />
                         </div>
                     </form>
                     <button className={`lg:hidden inline-flex items-center rounded-lg font-inter font-medium text-sm text-gray-800 p-2 ${filterOpen ? `bg-green-300 focus:outline-none focus:ring-2 focus:ring-gray-200` : `hover:bg-green-100`}`} type="button" onClick={() => setFilterOpen(!filterOpen)}>
@@ -43,7 +52,16 @@ const Filter = () => {
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                                 </svg>
                             </div>
-                            <input className="block bg-gray-50 border border-gray-300 rounded-lg pl-10 p-2.5 py-2 w-full placeholder:font-inter placeholder:text-sm placeholder:text-gray-900/50" id="search-bar" name="search-bar" type="search" placeholder="Search" />
+                            <input
+                                className="block bg-gray-50 border border-gray-300 rounded-lg pl-10 p-2.5 py-2 w-full placeholder:font-inter placeholder:text-sm placeholder:text-gray-900/50"
+                                id="search-bar"
+                                name="search-bar"
+                                type="search"
+                                placeholder="Search"
+                                value={props.searchValue}
+                                onChange={(e) => props.onSearchBarChange(e.target.value)}
+                                disabled
+                            />
                         </div>
                         <button className="inline-flex items-center bg-blue-700 border border-blue-700 rounded-lg font-inter font-medium text-sm text-white p-2.5 ml-2" type="submit">
                             <svg className="mr-2 size-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -55,14 +73,30 @@ const Filter = () => {
                 </div>
                 <div className={`${filterOpen ? `` : `hidden`} lg:block lg:mt-0 mt-4`}>
                     <h3 className="font-inter font-medium text-lg text-gray-900 mb-3">Choose type:</h3>
-                    <ul className="grid sm:grid-cols-4 md:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-8 grid-cols-3 gap-4 font-inter w-full">
+                    <ul className="grid sm:grid-cols-4 md:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-4 grid-cols-3 gap-4 font-inter w-full">
                         {types.map((type, index) => (
                             <li key={index}>
-                                <input type="checkbox" id={`${type.typeName}-type-option`} name="type" value={type.typeName} className="hidden peer" onChange={(e) => handleTypeChange(e.target.value)} />
+                                <input type="checkbox" id={`${type.typeName}-type-option`} name="type" value={type.typeName} className="hidden peer" onChange={(e) => handleFilterChange("type", e.target.value)} />
                                 <label className="inline-flex items-center justify-between bg-white border-2 border-gray-200 rounded-lg text-gray-500 p-1 w-full cursor-pointer peer-checked:border-blue-600 hover:text-gray-600 peer-checked:text-gray-600 hover:bg-gray-50" htmlFor={`${type.typeName}-type-option`}>
                                     <div className="flex items-center">
                                         <Image src={type.typeIconSrc} alt={type.typeIconAlt} className="lg:size-8 size-6" />
                                         <div className="text-sm font-semibold lg:ms-2 w-full capitalize">{type.typeName}</div>
+                                    </div>
+                                </label>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <div className={`lg:block lg:mt-0 mt-4`}>
+                    <h3 className="font-inter font-medium text-lg text-gray-900 mb-3">Choose generation:</h3>
+                    <ul className="grid sm:grid-cols-4 md:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-4 grid-cols-3 gap-4 font-inter w-full">
+                        {generations.map((generation, index) => (
+                            <li key={index}>
+                                <input type="checkbox" id={`${generation.genName}-gen-option`} name="gen" value={generation.genId} className="hidden peer" onChange={(e) => handleFilterChange("gen", e.target.value)} />
+                                <label className="inline-flex items-center justify-between bg-white border-2 border-gray-200 rounded-lg text-gray-500 p-1 w-full cursor-pointer peer-checked:border-blue-600 hover:text-gray-600 peer-checked:text-gray-600 hover:bg-gray-50" htmlFor={`${generation.genName}-gen-option`}>
+                                    <div className="flex items-center">
+                                        <Image src={generation.genIconSrc} alt={generation.genIconAlt} className="lg:size-8 size-6" />
+                                        <div className="text-sm font-semibold lg:ms-2 w-full capitalize">{generation.genName}</div>
                                     </div>
                                 </label>
                             </li>
