b30f0330245ccaa1bed34cf687c51fc7df2cc9f6
TerSimaN <mcsiman998@gmail.com>
2024-06-24 23:33:49 +0300

    Added Moves table

---
+++ src/components/layout/Tables/Moves.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import AccordionHeader from "@/components/ui/AccordionHeader";
+import Link from "next/link";
+import { useState } from "react";
+
+const Moves = (props: { data: PokeAPI.Utility.NamedAPIResource[] }) => {
+    const [open, setOpen] = useState(false);
+
+    return (
+        <>
+            <button className="w-full" type="button" onClick={() => setOpen(!open)}>
+                <AccordionHeader text="Moves" isOpen={open} />
+            </button>
+            <div className={`${open ? `` : `hidden`}`}>
+                <div className="overflow-x-auto shadow-md border-x sm:rounded-b-lg">
+                    <table className="font-inter text-sm text-left text-gray-500 whitespace-nowrap w-full">
+                        <thead className="bg-gray-50 text-xs text-gray-700 uppercase">
+                            <tr>
+                                <th scope="col" className="px-6 py-3">
+                                    Move name
+                                </th>
+                                <th scope="col" className="px-6 py-3">
+                                    Move url
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {props.data.map((move, index) => (
+                                <tr key={index} className="odd:bg-white even:bg-gray-50 border-b">
+                                    <th scope="row" className="font-medium text-gray-900 px-6 py-4 capitalize">
+                                        {move.name}
+                                    </th>
+                                    <td className="px-6 py-4">
+                                        <Link href={move.url} className="font-medium text-blue-600 hover:underline">
+                                            {move.url}
+                                        </Link>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Moves
