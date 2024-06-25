92bb89e4b93ddc639abea8800c8aaf1034957382
TerSimaN <mcsiman998@gmail.com>
2024-06-25 20:27:10 +0300

    Added new function

--- src/lib/actions/generation.tsx
+++ src/lib/actions/generation.tsx
@@ -1,4 +1,5 @@
 import { poke_api_url } from "@/config/constants";
+import { getPokemonSpeciesByName, getVarietiesMap } from "./pokemonSpecies";
 
 export async function getGenerationList() {
     let response = await fetch(`${poke_api_url}/generation`);
@@ -16,6 +17,42 @@ export async function getGenerationById(id: string) {
     let response = await fetch(`${poke_api_url}/generation/${id}`);
     let data = (await response.json()) as PokeAPI.Game.Generation;
     return data;
+}
+
+export async function getGenPokemonMap(filter: string | string[]) {
+    const pokemonMap = new Map<string, PokeAPI.Utility.NamedAPIResource>();
+    const pokemonSpeciesNames: string[] = [];
+
+    if (Array.isArray(filter)) {
+        console.log("Filter as array:", filter);
+        for (const filterObj of filter) {
+            let generation = (await getGenerationById(filterObj)) as PokeAPI.Game.Generation;
+            generation.pokemon_species.forEach((pokemonSpecies) => {
+                if (!pokemonSpeciesNames.includes(pokemonSpecies.name)) {
+                    pokemonSpeciesNames.push(pokemonSpecies.name);
+                }
+            });
+        }
+    } else if (filter != '') {
+        console.log("Filter as string:", filter);
+        let generation = (await getGenerationById(filter)) as PokeAPI.Game.Generation;
+        generation.pokemon_species.forEach((pokemonSpecies) => {
+            if (!pokemonSpeciesNames.includes(pokemonSpecies.name)) {
+                pokemonSpeciesNames.push(pokemonSpecies.name);
+            }
+        });
+    }
+
+    for (const name of pokemonSpeciesNames) {
+        const varietiesMap = (await getVarietiesMap(name)) as Map<string, PokeAPI.Utility.NamedAPIResource>;
+        varietiesMap.forEach((value, key) => {
+            if (!pokemonMap.has(key)) {
+                pokemonMap.set(key, value);
+            }
+        });
+    }
+
+    return pokemonMap;
 }
 
 export async function getGenerationPokemonSpeciesMap(filter: string | string[]) {
